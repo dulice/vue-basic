1ffcaafa9df1d4335812617e14a844f1e2b4e4cc
dulice <duliceellen600@gmail.com>
2023-07-06 08:53:24 -0700

    teleport to other element

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,25 @@ import Modal from './components/Modal.vue'
 </script>
 <template>
   <h3>{{ title }}</h3>
-  <div v-if="showModal">
-    <Modal @close="toggleModal">
-      <template v-slot:header>
-        <h1>Sign up for news letter</h1>
-      </template>
-      <template v-slot:content>
-        <p>Get update information and promo code</p>
-      </template>
-      <template v-slot:actions>
-        <button @click="toggleModal">Ok</button>
-      </template>
-    </Modal>
-  </div>
+  <Teleport to="#modals">
+    <div v-if="showModal">
+      <Modal @close="toggleModal">
+        <template v-slot:header>
+          <h1>Sign up for news letter</h1>
+        </template>
+        <template v-slot:content>
+          <p>Get update information and promo code</p>
+        </template>
+        <template v-slot:actions>
+          <button @click="toggleModal">Ok</button>
+        </template>
+      </Modal>
+    </div>
   
-  <div v-if="showModal2">
-    <Modal @close="showModal2 = false"/>
-  </div>
+    <div v-if="showModal2">
+      <Modal @close="showModal2 = false"/>
+    </div>
+  </Teleport>
   <button @click="toggleModal">Show Signup Modal</button>
   <button @click="showModal2 = !showModal2">Show Default Modal</button>
 </template>
